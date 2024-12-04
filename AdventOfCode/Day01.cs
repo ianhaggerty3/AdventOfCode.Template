e3cd5197513ef024380d0836c0eb81cccd3f0966
Ian Haggerty <ianhaggerty3@gmail.com>
2024-12-03 22:11:06 -0500

    Solve day 1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode;
+﻿using MiscUtil.IO;
+
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
@@ -9,7 +11,51 @@ public class Day01 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        List<int> firstList = new();
+        List<int> secondList = new();
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        foreach (var line in new LineReader(() => new StringReader(_input)))
+        {
+            var parts = line.Split(" ").Where(p => p.Length != 0).ToArray();
+
+            firstList.Add(int.Parse(parts[0]));
+            secondList.Add(int.Parse(parts[1]));
+        }
+        
+        firstList.Sort();
+        secondList.Sort();
+
+        var total = 0;
+        foreach (var (first, second) in firstList.Zip(secondList))
+        {
+            total += Math.Abs(first - second);
+        }
+        
+        return new ValueTask<string>($"{total}");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        List<int> firstList = new();
+        List<int> secondList = new();
+
+        foreach (var line in new LineReader(() => new StringReader(_input)))
+        {
+            var parts = line.Split(" ").Where(p => p.Length != 0).ToArray();
+
+            firstList.Add(int.Parse(parts[0]));
+            secondList.Add(int.Parse(parts[1]));
+        }
+
+        var total = 0;
+        foreach (var number in firstList)
+        {
+            var count = secondList.Count(n => n == number);
+            total += number * count;
+        }
+
+        return new ValueTask<string>($"{total}");
+    }
 }
